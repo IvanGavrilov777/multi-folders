4357b48179e1418267189fa08524f75c5f081056
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-03-20 11:50:03 +0200

    Update import.tf

--- import/import.tf
+++ import/import.tf
@@ -1,6 +1,31 @@
-/*
-import {
-  to = module.m2.random_id.r3
-  id = "9nR33M-8VLY"
+provider "aws" {
+    region = "us-west-1"
 }
-*/
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_key_pair" "search_for_key_pair" {}
+
+resource "aws_instance" "VM" {
+  #count = 3
+  instance_type = "t2.micro"
+  ami = data.aws_ami.ubuntu.id
+  key_name = data.aws_key_pair.search_for_key_pair.key_name
+tags = {
+  Name = "testVMs"
+}
+}
