7442202c08f4a6ee7abe33b56807cedb6e9b1a4e
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-03-27 14:19:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,20 @@ resource "random_id" "r1" {
 }
 
 module "m1" {
-source = "./import"
+source = local.im
 }
+variable "po" {
+default = "po"
+}
+
+variable "rt" {
+default = "rt"
+}
+
+locals {
+im = "./im${var.po}${var.rt}"
+}
+
 
 import {
   to = module.m1.aws_instance.VM
